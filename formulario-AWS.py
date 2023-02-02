9369a3cecae884ebcdbcd0af8454363ff9d12811
Jose Cazorla <40724062+JCazorla90@users.noreply.github.com>
2023-02-02 18:29:48 +0100

    Update formulario-AWS.py
    En esta versión, se utiliza una función get_input que valida el ingreso del usuario y muestra un mensaje de error si es necesario. Esta función es reutilizable para validar los inputs de los campos requeridos.

--- formulario-AWS.py
+++ formulario-AWS.py
@@ -1,50 +1,42 @@
 import os
 
-file_path = input("Ingresa la ruta del archivo: ")
+def get_input(prompt, valid_options=None):
+    """Get user input with validation."""
+    while True:
+        value = input(prompt).strip()
+        if valid_options is not None and value not in valid_options:
+            print(f"Error: opción inválida. Las opciones válidas son: {', '.join(valid_options)}")
+        else:
+            return value
 
-region = input("Ingresa la región deseada: ")
-ami = input("Ingresa la AMI: ")
-vpc_id = input("Ingresa el ID del VPC: ")
-subnet_id = input("Ingresa el ID de la subred: ")
-instance_type = input("Ingresa el tipo de instancia: ")
-num_instances = int(input("Ingresa el número de máquinas: "))
-persistent = input("La infraestructura es persistente (sí o no): ").lower() == "sí"
+file_path = get_input("Ingresa la ruta del archivo: ")
 
-instance_count = "count = {}".format(num_instances) if persistent else ""
+region = get_input("Ingresa la región deseada: ")
+ami = get_input("Ingresa la AMI: ")
+vpc_id = get_input("Ingresa el ID del VPC: ")
+subnet_id = get_input("Ingresa el ID de la subred: ")
+instance_type = get_input("Ingresa el tipo de instancia: ")
 
 template = f"""provider "aws" {{
-region = "{region}"
+  region = "{region}"
 }}
 
 resource "aws_vpc" "example" {{
-id = "{vpc_id}"
+  id = "{vpc_id}"
 }}
 
 resource "aws_subnet" "example" {{
-vpc_id = aws_vpc.example.id
-cidr_block = "{subnet_id}"
+  vpc_id = aws_vpc.example.id
+  cidr_block = "{subnet_id}"
 }}
 
 resource "aws_instance" "example" {{
-{instance_count}
-ami = "{ami}"
-instance_type = "{instance_type}"
-subnet_id = aws_subnet.example.id
-}}
-
-resource "aws_s3_bucket" "example" {{
-bucket = "terraform-states-{vpc_id}"
-}}
-
-terraform {
-backend "s3" {{
-bucket = aws_s3_bucket.example.bucket
-key = "terraform.tfstate"
-region = "{region}"
-}}
-}"""
+  ami           = "{ami}"
+  instance_type = "{instance_type}"
+  subnet_id     = aws_subnet.example.id
+}}"""
 
 with open(file_path, "w") as f:
-f.write(template)
+    f.write(template)
 
 print("Archivo Terraform creado exitosamente en:", file_path)
